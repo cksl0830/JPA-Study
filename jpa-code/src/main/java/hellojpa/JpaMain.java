a51c64e09e79d1004a9e3f00b6008be7b5a1b751
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-05-09 21:29:27 +0900

    도메인 설계

--- jpa-code/src/main/java/hellojpa/JpaMain.java
+++ jpa-code/src/main/java/hellojpa/JpaMain.java
@@ -14,17 +14,6 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction(); //jpa 변경은 모두 트랜잭션 안에서!
         tx.begin();
         try {
-//            Member member1 = new Member(1L, "chan");
-//            Member member2 = new Member(2L, "yunha");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-            Member member = em.find(Member.class,1L);
-            member.setName("zz");
-//
-
-
-            //디비에 저장되는 시점
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
