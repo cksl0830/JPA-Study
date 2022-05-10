63c73c89c9eec766c7d52fb6e8cc033242798885
Park Chan <60590737+cksl0830@users.noreply.github.com>
2022-05-10 15:08:01 +0900

    일대다 , 다대일 매핑 추가

---
+++ jpa-code/src/main/java/hellojpa/domain/Category.java
@@ -0,0 +1,25 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+//    @ManyToMany  // 잘 안씀 , 보기만 하기 !!!  중간테이블 만들어서 진행
+//    @JoinTable(name = "CATEGOTY_ITEM")
+//    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name="PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
---
+++ jpa-code/src/main/java/hellojpa/domain/Delivery.java
@@ -0,0 +1,15 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+}
